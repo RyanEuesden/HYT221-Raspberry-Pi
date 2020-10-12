ee66a1467d97b10422e6b7d472c75dba1e5bd316
RyanEuesden <70282587+RyanEuesden@users.noreply.github.com>
2020-10-13 00:21:26 +0100

    Update temphumid.py

--- temphumid.py
+++ temphumid.py
@@ -16,17 +16,17 @@ MINUTES_BETWEEN_READS = 0.1
 def gethumid():
 	# Get I2C bus
 	bus = smbus.SMBus(1)
+	address = 0x28
 
-	# HYT221 address, 0x28(40)
-	#		0x80(128)	Send normal mode
-	bus.write_byte(0x28, 0x80)
-
-	time.sleep(0.5)
+	# Initialise
+	void = bus.write_quick(address)
+    	
+	time.sleep(0.1)
 
 	# HYT221 address, 0x28(40)
 	# Read data back from 0x00(00), 4 bytes
 	# Humidity MSB, Humidity LSB, Temp MSB, Temp LSB
-	data = bus.read_i2c_block_data(0x28, 0x00, 4)
+	data = bus.read_i2c_block_data(0x28, 0, 4)
 
 	# Convert the data to 14-bits
 	humidity = ((data[0] & 0x3F) * 256 + data[1]) * (100 / 16383.0)
@@ -36,12 +36,12 @@ def gethumid():
 def gettemp():
         # Get I2C bus
         bus = smbus.SMBus(1)
-
-        # HYT221 address, 0x28(40)
-        #               0x80(128)       Send normal mode
-        bus.write_byte(0x28, 0x80)
-
-        time.sleep(0.5)
+	address = 0x28
+        
+	# Initialise
+        void = bus.write_quick(address)
+    	
+	time.sleep(0.1)
 
         # HYT221 address, 0x28(40)
         # Read data back from 0x00(00), 4 bytes
