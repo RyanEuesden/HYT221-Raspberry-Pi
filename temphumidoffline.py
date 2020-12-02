6fc37a04b24662758194672c4f27084476debc55
RyanEuesden <70282587+RyanEuesden@users.noreply.github.com>
2020-12-02 19:04:25 +0000

    Create temphumidoffline.py

---
+++ temphumidoffline.py
@@ -0,0 +1,51 @@
+# HYT221 Inital State Script
+
+#import necessary modules
+import smbus
+import time
+import csv
+import os.path
+
+# Check if csv log already exists
+file_exists = os.path.isfile('/home/pi/Downloads/HYT221-Raspberry-Pi/temphumidrawlog.csv')
+
+#define csv headers
+headers = ['Date', 'Time', 'Temperature', 'Humidity']
+
+# Create csv file processing loop (writes headers only if no file created. If file already exists then new data appended to end of existing file
+def processing_loop(csvfile):
+        csv_writer = csv.writer(csvfile)
+        if not file_exists:
+                csv_writer.writerow(headers)
+
+
+        while True:
+
+                # Get I2C bus
+                bus = smbus.SMBus(1)
+                address = 0x28
+
+                # Initialise
+                void = bus.write_quick(address)
+
+                time.sleep(0.1)
+
+                # HYT221 address, 0x28(40)
+                # Read data back from 0x00(00), 4 bytes
+                # Humidity MSB, Humidity LSB, Temp MSB, Temp LSB
+                data = bus.read_i2c_block_data(0x28, 0, 4)
+
+                # Read data bytes and convert to decimal
+                humidity = ((data[0] & 0x3F) * 256 + data[1]) * (100 / 16383.0)
+                cTemp = ((data[2] * 256 + (data[3] & 0xFC)) / 4) * (165 / 16383.0) - 40
+		
+		# Get date and Time
+                Date = time.strftime('%d-%m-%Y')
+                Time = time.strftime('%H:%M.%S')
+
+                # Write Data to CSV File
+		csv_writer.writerow([Date, Time, cTemp, humidity])
+                csvfile.flush()
+
+with open ('temphumidrawlog.csv', 'a+') as csvfile:
+        processing_loop(csvfile)
